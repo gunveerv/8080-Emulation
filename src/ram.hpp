67ee3bbf2d3f923dfa70670476e3d2f7bd8cdf55
Gunveer Vilkhu <gvilkhu@uwo.ca>
2024-09-02 15:12:08 -0400

    added ram

---
+++ src/ram.hpp
@@ -0,0 +1,23 @@
+#ifndef RAM_H
+#define RAM_H
+
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <iomanip> // for std::setw
+// #include <sstream> // for std::ostringstream
+#include "constants.hpp"
+
+class Disassembler
+{
+     private: 
+          unsigned char codeBuffer[MAX_ROM_SIZE];
+     public:
+          Disassembler();
+          ~Disassembler();
+          int disassembleRom(std::string romPath);
+          int disassembleInstruction(unsigned char* buffer, int* pc);
+          std::string disassemblerToStringHex(unsigned char* code);
+};
+ 
+#endif
